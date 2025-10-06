ed2c4baa3b02d3c5fc31094701fe27ae129bd070
maria_zhiganova <my.nik.mariann@gmail.com>
2025-10-07 00:48:19 +0300

    add admin in mail_flow

--- mail_flow/admin.py
+++ mail_flow/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from mail_flow.models import Recipient, Message, Mailing, MailingAttempt
+
+
+@admin.register(Recipient)
+class RecipientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'full_name', 'comment')
+    list_filter = ('full_name', 'email')
+    search_fields = ('full_name', 'email')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'body')
+    list_filter = ('subject',)
+    search_fields = ('subject',)
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('start_time', 'end_time', 'status', 'message')
+    list_filter = ('start_time', 'status', 'end_time')
+    search_fields = ('start_time', 'status', 'end_time')
+
+
+@admin.register(MailingAttempt)
+class MailingAttemptAdmin(admin.ModelAdmin):
+    list_display = ('start_time', 'status', 'post_response', 'mailing')
+    list_filter = ('start_time', 'status', 'post_response',)
+    search_fields = ('start_time', 'status', 'post_response',)
